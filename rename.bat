bac311e9881d8abd5432741ac00d28249d1eacd6
DanoR <zheung@vip.qq.com>
2019-10-10 14:09:41 +0800

    247

--- rename.bat
+++ rename.bat
@@ -1,6 +1,4 @@
-rm zh_cn.pre.txt
-rm en_us.pre.txt
-mv zh_cn.txt zh_cn.pre.txt
-mv en_us.txt en_us.pre.txt
-mv 2D845C67A39BD6FE zh_cn.txt
-mv CC7248B565172CE2 en_us.txt
+move zh_cn.txt zh_cn.pre.txt
+move en_us.txt en_us.pre.txt
+move 2D845C67A39BD6FE zh_cn.txt
+move CC7248B565172CE2 en_us.txt
